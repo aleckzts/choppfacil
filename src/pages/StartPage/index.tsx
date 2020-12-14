abef7cdb57768435ed6d248f0c64d21f661a6b7a
Aleck Zander <aleckzts@gmail.com>
2020-12-13 22:56:27 -0300

    start brand listing

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -1,17 +1,72 @@
 import React from 'react';
+
 import { Content } from '../styles';
+import { Brands, HeaderText } from './styles';
+
+const brands = [
+  'Adriática',
+  'Antarctica Sub Zero',
+  'Antarctica',
+  "Beck's",
+  'Brahma',
+  'Brahma Zero',
+  'Bohemia',
+  'Brahma Light',
+  'Brahma Extra',
+  'Brahma Bier',
+  'Brahma Fresh',
+  'Budweiser',
+  'Caracu',
+  'Colorado',
+  'Corona',
+  'Franziskaner',
+  'Goose Island',
+  'Hertog Jan',
+  'Hoegaarden',
+  'Kronenbier',
+  'Labatt Blue',
+  'Lakeport Brewing',
+  'Leffe',
+  'Magnífica',
+  'Miller',
+  'Norteña',
+  'Nossa',
+  'Original',
+  'Patagonia',
+  'Patricia',
+  'Pilsen',
+  'Polar',
+  'President',
+  'Puerto del Mar',
+  'Quilmes',
+  'Serramalte',
+  'Serrana',
+  'Skol',
+  'Skol Hops',
+  'Skol 360',
+  'Skol Beats Senses',
+  'Skol Beats Spirit',
+  'Skol Beats Secret',
+  'Skol Beats 150 BPM',
+  'Skol Beats GT',
+  'Skol Ultra',
+  'Skol Puro Malte',
+  'Samson',
+  'Stella Artois',
+  'Wäls',
+];
 
 const StartPage: React.FC = () => {
   return (
     <Content>
-      <strong>
-        Lorem ipsum vitae molestie scelerisque a augue sit donec netus
-        suspendisse
-      </strong>
-      <strong>
-        Lorem ipsum vitae molestie scelerisque a augue sit donec netus
-        suspendisse
-      </strong>
+      <HeaderText>
+        <strong>Escolha a marca que você costuma consumir</strong>
+      </HeaderText>
+      <Brands>
+        {brands.map(name => (
+          <p>{name}</p>
+        ))}
+      </Brands>
     </Content>
   );
 };
---
+++ src/pages/StartPage/styles.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const HeaderText = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  text-align: center;
+
+  width: 100%;
+  min-height: 360px;
+  flex-wrap: wrap;
+
+  strong {
+    font-size: 24px;
+    margin: 16px 0;
+  }
+`;
+
+export const Brands = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  text-align: center;
+
+  width: 100%;
+  min-height: 360px;
+  flex-wrap: wrap;
+
+  strong {
+    font-size: 24px;
+    margin: 16px 0;
+  }
+`;
