1d9c20c943e5839e094c6e3b8e388599ca5efe2d
Aleck Zander <aleckzts@gmail.com>
2020-12-13 23:05:31 -0300

    header layout

--- src/pages/StartPage/styles.tsx
+++ src/pages/StartPage/styles.tsx
@@ -5,11 +5,9 @@ export const HeaderText = styled.div`
   flex: 1;
   flex-direction: column;
   align-items: center;
-  /* justify-content: center; */
   text-align: center;
 
   width: 100%;
-  min-height: 360px;
   flex-wrap: wrap;
 
   strong {
